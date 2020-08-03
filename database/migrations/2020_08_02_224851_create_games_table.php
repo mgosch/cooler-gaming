42fa6ed52f4a69439868384c19cdd8522108473d
Vicky <maviyiyi@gmail.com>
2020-08-02 21:11:30 -0300

    se agrega tabla de juegos

---
+++ database/migrations/2020_08_02_224851_create_games_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->string('name');
+            $table->string('description');
+            $table->string('state');
+            $table->float('percentaje_rent');
+            $table->float('amount');
+            $table->float('reward_cooler_coins');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
