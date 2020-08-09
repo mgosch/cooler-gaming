326d2204a1cd1bc392beb15060dc6d8e36ce049b
Archi <alejandro_19873000@hotmail.com>
2020-08-08 21:11:48 -0300

    Buscador funcional con género y nombre

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,9 +9,11 @@ class GameController extends Controller
 {
     public function inicio(Request $request) {
         $gameName = $request->input('name');
+        $gameGenre = $request->input('genre');
         $games = DB::table('games')->join('game_genres', 'games.id' , 'game_genres.game_id')
                                    ->join('genres', 'game_genres.genres_id' , 'genres.id')
                                    ->where('games.name', 'LIKE', '%'.$gameName.'%')
+                                   ->where('genres.description', 'LIKE', $gameGenre)
                                    ->get(['games.name', 'games.description', 'games.image', 'genres.description as genero']);
         return view('welcome', compact('games'));
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,6 +117,14 @@
                 <nav class="navbar navbar-light bg-light">
                     <form class="form-inline">
                         <input class="form-control mr-sm-2" style="width: 700px;" type="search" name="name" placeholder="Buscar" aria-label="Search">
+                        <select name="genre" id="genre">
+                            <option value="">Todos</option>
+                            <option value="Carrera">Carrera</option>
+                            <option value="Aventura">Aventura</option>
+                            <option value="Acción">Acción</option>
+                            <option value="Simulación">Simulación</option>
+                            <option value="Estrategia">Estrategia</option>
+                        </select>
                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
                     </form>
                </nav>
