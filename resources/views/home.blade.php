dbd68a1191460f50b682278c7ce8a4717dc921c6
Archi <alejandro_19873000@hotmail.com>
2020-08-13 20:04:38 -0300

    Se "VUELVEN" a arreglar los formatos de las cards de juegos (gracias vicky)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="container">
+
+<link rel="stylesheet" href="<?php echo asset('css/style.css')?>" type="text/css"> 
+
+    <div class="container">
         <nav class="navbar navbar-light bg-light">
-                            <form class="form-inline">
-                                <input class="form-control mr-sm-2" style="width: 700px;" type="search" name="name" placeholder="Buscar" aria-label="Search">
-                                <select name="genre" id="genre" style= "margin-right: 10px;">
-                                    <option value="">Todos</option>
-                                    <option value="Carrera">Carrera</option>
-                                    <option value="Aventura">Aventura</option>
-                                    <option value="Acción">Acción</option>
-                                    <option value="Simulación">Simulación</option>
-                                    <option value="Estrategia">Estrategia</option>
-                                </select>
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                            </form>
-                    </nav>
-                    <br>
-                        <div class="row">
-                            <div class="col-md-6 col-sm-6" style= "display: contents;">
-                                @foreach($games as $item)
-                                <div class="block-4 text-center" style= "width: 20rem; margin-right: 10px;">
-                                    <img src="images/{{$item->image}}" class="card-img-top" alt="image">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{$item->name}}</h5>
-                                        <p class="card-text">{{$item->description}}</p>
-                                        <a href="{{route('alquilar', $item->id)}}" class="btn btn-primary">Alquilar</a>
-                                    </div>
-                                </div>
-                                @endforeach()
-                            </div>
-                        </div>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" style="width: 700px;" type="search" name="name" placeholder="Buscar" aria-label="Search">
+                <select name="genre" id="genre" style= "margin-right: 10px;">
+                    <option value="">Todos</option>
+                    <option value="Carrera">Carrera</option>
+                    <option value="Aventura">Aventura</option>
+                    <option value="Acción">Acción</option>
+                    <option value="Simulación">Simulación</option>
+                    <option value="Estrategia">Estrategia</option>
+                </select>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+        </nav>
+        
+        <br>
+
+        <div class="row">
+            <div class="col-md-6">
+                @foreach($games as $item)
+                <div class="contenido-lindo">
+                    <img src="images/{{$item->image}}" class="card-img-top" alt="image">
+                    <div class="card-body-top">
+                        <h5 class="card-title">{{$item->name}}</h5>
+                        <p class="card-text">{{$item->description}}</p>
+                    </div>
+                    <div class="card-body-bottom">
+                            <a href="{{route('alquilar', $item->id)}}" class="btn btn-primary">Alquilar</a>
+                    </div>
+                    
+                </div>
+                @endforeach()
+            </div>
+        </div>
         </div>
 
 @endsection
